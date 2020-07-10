5e2d73430e0c4e88c626d376d4f0df4b5564e870
Sarkus, Edvinas <edvinas.sarkus@zuhlke.com>
2020-07-10 11:08:36 +0100

    Initial Commit

---
+++ ExpandableCustomView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExpandableCustomView
+//
+//  Created by Edvinas Sarkus on 10/07/2020.
+//  Copyright © 2020 Edvinas Sarkus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
